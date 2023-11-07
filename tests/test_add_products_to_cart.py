e4a5a26c8db96151e354510ed79d0f5fedfc6583
Tati-Ramos <tatiane_sr@outlook.com>
2023-11-07 14:09:45 -0300

    order completion

--- tests/test_add_products_to_cart.py
+++ tests/test_add_products_to_cart.py
@@ -47,5 +47,31 @@ time.sleep(1)
 assert driver.find_element(By.XPATH, "//*[@class='inventory_item_name' and text()='Sauce Labs Bike Light']").is_displayed()
 time.sleep(1)
 
+#checkout
+driver.find_element(By.ID, "checkout").click()
+assert driver.find_element(By.XPATH, "//span[@class='title' and text()='Checkout: Your Information']")
+time.sleep(1)
+first_name = driver.find_element(By.ID,"first-name").send_keys("Teste")
+time.sleep(1)
+last_name = driver.find_element(By.ID, "last-name").send_keys("Funcional")
+time.sleep(1)
+postal_code = driver.find_element(By.ID, "postal-code").send_keys("24472290")
+
+continue_btn = driver.find_element(By.ID, "continue").click()
+assert driver.find_element(By.XPATH, "//span[@class='title' and text()='Checkout: Overview']")
+time.sleep(2)
+
+finish_btn = driver.find_element(By.ID, "finish").click()
+assert driver.find_element(By.XPATH, "//span[@class='title' and text()='Checkout: Complete!']")
+assert driver.find_element(By.XPATH, "//h2[@class='complete-header' and text()='Thank you for your order!']")
+time.sleep(2)
+
+
+
+
+
+
+
+
 
 driver.quit()
